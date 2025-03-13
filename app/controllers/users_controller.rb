6b9b01ac7c7791aaaf13140bd46d963dc87c51ab
Joao Almeida <aalmeidadev@gmail.com>
2025-03-13 19:12:10 -0300

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def index
     users = User
-              .by_company(params[:company_identifier])
+              .by_company(search_params[:company_identifier])
               .by_username(search_params[:username])
     render json: users
   end
